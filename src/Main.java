6977f7c104a60bb431105249fd42c411cb196554
marcos <ilejbr@gmail.com>
2022-11-16 12:47:36 +0000

    version 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Escreva o número Fibonacci :");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        int n1 = 0, n2 = 1, n3, i, count = num;
+        System.out.print(n1 + " " + n2);
+        for (i = 2; i < count; ++i)
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
     }
 }
